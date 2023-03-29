4d10b815b93babbce98e6f988a988fd6b208f9f5
anupamdas1511 <dasanupam151101@gmail.com>
2023-03-30 01:13:25 +0530

    home routes added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import "./App.css";
+import { Route, Routes } from 'react-router-dom'
+import { Home } from './components'
 
 function App() {
 
   return (
-    <div>
-      
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
     </div>
   );
 }
