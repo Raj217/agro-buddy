98c1c0501062b92e9f35d3ef6723eb9c06a97214
anupamdas1511 <dasanupam151101@gmail.com>
2023-03-31 01:48:55 +0530

    min length added to user password

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,23 @@
-import { Schema, model } from 'mongoose';
-import UserRoles from './constants.js';
+import { Schema, model } from "mongoose";
+import UserRoles from "./constants.js";
+import Validators from "../utils/Validators.js";
 
-const userSchema = new Schema({
+const userSchema = new Schema(
+  {
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true },
-    password: { type: String, required: true },
-    role: { type: String, required: true, enum: UserRoles }
-}, {timestamps: true});
+    password: {
+      type: String,
+      required: true,
+      validate: {
+        validator: Validators.validateMinPasswordSize,
+        message: "size must be at least 6",
+      },
+    },
+    role: { type: String, required: true, enum: UserRoles },
+  },
+  { timestamps: true }
+);
 
 export default model("User", userSchema);
--- backend/utils/Validators.js
+++ backend/utils/Validators.js
@@ -1,8 +1,16 @@
 class Validators {
-    static isValidEmail(email) {
-        const re = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)
-        return email.match(re) !== null
-    }
+  static isValidEmail(email) {
+    const re = new RegExp(
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
+    );
+    return email.match(re) !== null;
+  }
+  static validateMinPasswordSize = (minPasswordSize) => {
+    const minPasswordSizeString = minPasswordSize + "";
+    return isInt(minPasswordSizeString, {
+      min: 6,
+    });
+  };
 }
 
-export default Validators
+export default Validators;
