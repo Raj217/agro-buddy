4f44c5d9f9f1b7f8d9d8b060fffa3dfdb8856e61
anupamdas1511 <dasanupam151101@gmail.com>
2023-04-03 14:43:57 +0530

    added api in frontend

---
+++ frontend/src/api/auth.js
@@ -0,0 +1,13 @@
+import API from './api'
+
+export const signup = (user) => API.post('/signup', user);
+
+export const login = (user) => API.post('/login', user);
+
+export const getUser = (id) => API.get(`/user/${id}`);
+
+export const forgotPassword = (email) => API.post('/forgot-password', email);
+
+export const resetPassword = (email) => API.post('/reset-password', email);
+
+export const generateOtp = (email) => API.post('/generate-otp', email);
