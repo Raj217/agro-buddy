b3f496d93102ca6313c75b2c2252188a2dc80622
Raj <63448767+Raj217@users.noreply.github.com>
2023-04-13 00:45:57 +0530

    Update SearchCrops.jsx

--- frontend/src/components/SearchCrops/SearchCrops.jsx
+++ frontend/src/components/SearchCrops/SearchCrops.jsx
@@ -61,59 +61,6 @@ function SearchCrops() {
         ))} */}
       </div>
     </Stack>
-  //   const handleSearch = async () => {
-  //       if (search) {
-  //           console.log(search);
-  //           const { data } = await getCropDetails(search);
-  //           setCropsData(data.crops);
-  //       }
-  //       return (
-  //         <Stack p="20px" alignItems="center" justifyContent="center" mt="37px">
-  //           <Box position="relative" mb="72px">
-  //             <TextField
-  //               sx={{
-  //                 input: {
-  //                   fontWeight: "700",
-  //                   outline: "none",
-  //                   borderRadius: "4px",
-  //                 },
-  //                 width: { lg: "800px", xs: "320px" },
-  //                 backgroundColor: "cream",
-  //                 borderRadius: "50px",
-  //               }}
-  //               height="76px"
-  //               onChange={(e) => setSearch(e.target.value.toLowerCase())}
-  //               placeholder="Search Crops"
-  //               type="text"
-  //             />
-      
-  //             <Button
-  //               className="search-btn"
-  //               variant="contained"
-  //               sx={{
-  //                 textTransform: "none",
-  //                 width: { lg: "120px", xs: "70px" },
-  //                 fontSize: { lg: "16px", xs: "12px" },
-  //                 height: "56px",
-  //                 position: "absolute",
-  //                 right: "0",
-  //                 borderRadius: "0",
-  //               }}
-  //               onClick={handleSearch}
-  //             >
-  //               Search
-  //             </Button>
-  //           </Box>
-  //           <div>
-  //             {cropsData.map((crop) => (
-  //               <SearchCard key={crop._id} data={crop} />
-  //             ))}
-  //           </div>
-  //         </Stack>
-  // );
-
-    // }
-
   )};
 
   
