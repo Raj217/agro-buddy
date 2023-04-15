5bf7f48214d0b71af3b83031de07a50a3d63b15b
Raj <rajdristant007@gmail.com>
2023-04-16 00:28:05 +0530

    added params-range route and implemented it

--- backend/routes/crop-routes.js
+++ backend/routes/crop-routes.js
@@ -6,6 +6,7 @@ const router = Router();
 router.post("", CropDetails.register);
 router.get("", CropDetails.getPreview);
 router.get("/details", CropDetails.getDetails);
+router.get("/params-range", CropDetails.getParamsRange);
 router.patch("", CropDetails.update);
 router.delete("", CropDetails.deleteCrop);
 router.put("/image", CropDetails.updateImage);
