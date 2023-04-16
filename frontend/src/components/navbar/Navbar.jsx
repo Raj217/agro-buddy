c8d8abd636ac055a4d68d36e5758581f08c9a016
Akash Suklabaidya <akashsuklabaidya593@gmail.com>
2023-04-16 21:10:47 +0530

    resolved conflicts

--- frontend/src/components/navbar/Navbar.jsx
+++ frontend/src/components/navbar/Navbar.jsx
@@ -3,14 +3,14 @@ import { useNavigate } from "react-router-dom";
 import MenuIcon from "@mui/icons-material/Menu";
 import CloseIcon from "@mui/icons-material/Close";
 import PersonIcon from "@mui/icons-material/Person";
-import { useTheme, Avatar } from "@mui/material";
+import { useTheme, Avatar, Button } from "@mui/material";
 import { AuthContext } from "../../context/auth";
 import { AppBar, Toolbar, Typography, CssBaseline } from "@mui/material";
 import Logo from "../../assets/logo.svg";
 import "./Navbar.css";
 import React from "react";
 
-function Navbar() {
+function Navbar({ signedIn, setSignedIn }) {
   const allNavLinks = [
     { name: "Home", link: "/" },
     { name: "About", link: "/about" },
@@ -27,8 +27,8 @@ function Navbar() {
   const [isIconClicked, setIsIconClicked] = React.useState(false);
   const navigate = useNavigate();
   const theme = useTheme();
-  const [isNotMobile, setIsMobile] = useState(theme.breakpoints.down("sm"));
-  const { loggedIn } = useContext(AuthContext);
+  let isNotMobile = theme.breakpoints.up("sm") !== null;
+  console.log(signedIn);
   const isMobile = theme.breakpoints.down("sm");
 
   return (
@@ -71,18 +71,21 @@ function Navbar() {
               ></div>
             </li>
           ))}
-          {loggedIn ? (
-            <Avatar sx={{ display: "none" }}>
-              <PersonIcon />
-            </Avatar>
-          ) : null}
+          {signedIn && (
+            <Button
+              onClick={() => {
+                localStorage.removeItem("token");
+                setSignedIn(false);
+                window.location.reload(false);
+              }}
+            >
+              Sign out
+            </Button>
+          )}
         </div>
         <div
           className={`nav-links-mobile ${!isIconClicked ? "icon-not-active" : "icon-active"
             }`}
-          style={{
-            display: isNotMobile && "none",
-          }}
         >
           {allNavLinks.map((link) => (
             <li
@@ -106,7 +109,6 @@ function Navbar() {
         <div
           className="mobileview"
           onClick={() => {
-            // setIsMobile((prev) => !prev);
             setIsIconClicked(!isIconClicked);
           }}
         >
