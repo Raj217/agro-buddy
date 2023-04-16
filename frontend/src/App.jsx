c117cfd33a98d1d19a9d66ace29317b011bf86af
Raj <rajdristant007@gmail.com>
2023-04-16 21:20:45 +0530

    bug fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,21 @@
-import React from 'react';
-import React from 'react';
+import React from "react";
+import React from "react";
 import "./App.css";
-import { Route, Routes } from 'react-router-dom'
-import { Home } from './components'
-import About from './pages/about/About'
-import Contact from './pages/contact/Contact'
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components";
+import About from "./pages/about/About";
+import Contact from "./pages/contact/Contact";
 import Navbar from "./components/navbar/Navbar";
 import SignIn from "./components/forms/SignIn";
 import SignUp from "./components/forms/SignUp";
 import GenerateOtp from "./pages/Otp/GenerateOtp";
 import Footer from "./components/footer/Footer";
 import ForgotPasswordParams from "./components/ForgotPassword/ForgotPasswordParams";
-import ForgotPasswordParams from "./components/ForgotPassword/ForgotPasswordParams";
 import ForgotPassword from "./components/ForgotPassword/ForgotPassword";
 import Charts from "./components/contents/Charts";
 import Content from "./components/content/Content";
 import Search from "./components/search/Search";
-import { AuthContext } from './context/auth';
-import { AuthContext } from './context/auth';
+import { AuthContext } from "./context/auth";
 
 function App() {
   const { loggedIn } = React.useContext(AuthContext);
@@ -26,7 +24,6 @@ function App() {
   return (
     <div className="App">
       <Navbar signedIn={signedIn} setSignedIn={setSignedIn} />
-      <Navbar signedIn={signedIn} setSignedIn={setSignedIn} />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
@@ -34,12 +31,14 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/otp" element={<GenerateOtp />} />
-        <Route path="/forgot-password/:token" element={<ForgotPasswordParams />} />
-        <Route path="/forgot-password/:token" element={<ForgotPasswordParams />} />
+        <Route
+          path="/forgot-password/:token"
+          element={<ForgotPasswordParams />}
+        />
         <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path='/details/:crop' element={<Content />} />
+        <Route path="/details/:crop" element={<Content />} />
         <Route path="/search" element={<Search />} />
-        <Route path="/developers" element={< Developer />} />
+        <Route path="/developers" element={<Developer />} />
       </Routes>
       <Footer />
     </div>
